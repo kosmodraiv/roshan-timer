f618a2bf36ecadd09af67405e46b041f56d488b7
kosmodraiv <60289387+kosmodraiv@users.noreply.github.com>
2023-02-07 02:30:28 +0200

    roshan-timer

---
+++ roshan.js
@@ -0,0 +1,27 @@
+var timermin = 0;
+var timermax = 0;
+
+function roshan(x){
+    var x;
+    do {
+      x = Number(prompt('Введите минуту'));
+    } while (!x);
+    timermin = x + 8
+    timermax = x + 11
+    var result = String("Min: " + timermin) + " " +  String("Max: " + timermax)
+
+    // Код для копирования результата в буфер обмена
+    const el = document.createElement('textarea');
+    el.value = result;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+
+    return result;
+};
+
+function resultToDiv() {
+    const result = roshan();
+    document.getElementById("result").innerHTML = result;
+};
